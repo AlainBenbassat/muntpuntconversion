797b3bfdf0899ee8db75726072744ab312f921ed
Alain Benbassat <alain@businessandcode.eu>
2021-11-17 10:20:39 +0100

    refactoring

--- start.php
+++ start.php
@@ -2,26 +2,29 @@
 
 require 'common.php';
 
-function main($task) {
-  $BATCH_LIMIT = 2500000;
+$validTasks = [
+  'score_source_contacts',
+  'mark_duplicates',
+  'convert_contacts',
+  'convert_events',
+  'convert_participants',
+  'all',
+];
 
+$BATCH_LIMIT = 2500000;
+
+function main() {
   try {
+    $task = getTask();
+
     loadClasses();
     bootstrapCiviCRM();
 
-    if ($task == 'score') {
-      //$scoreGenerator = new SourceContactScoreGenerator($BATCH_LIMIT);
-      //$scoreGenerator->validateAllContacts();
-
-      $duplicateFinder = new SourceContactDuplicateFinder();
-      $duplicateFinder->markMainContacts();
-
-      $logger = new SourceContactLogger();
-      $logger->printStats();
+    if ($task == 'all') {
+      executeAllTasks();
     }
-    elseif ($task == 'convert') {
-      $convertor = new Convertor($BATCH_LIMIT);
-      $convertor->start();
+    else {
+      executeTask($task);
     }
   }
   catch (Exception $e) {
@@ -35,26 +38,86 @@ function main($task) {
   echo "\nDone.\n";
 }
 
+function executeTask($task) {
+  if (!function_exists($task)) {
+    throw new Exception("Function $task doesn't exist.");
+  }
+
+  call_user_func($task);
+}
+
+function executeAllTasks() {
+  global $validTasks;
+
+  foreach ($validTasks as $task) {
+    if ($task != 'all') {
+      executeTask($task);
+    }
+  }
+}
+
+function score_source_contacts() {
+  global $BATCH_LIMIT;
+
+  $scoreGenerator = new SourceContactScoreGenerator($BATCH_LIMIT);
+  $scoreGenerator->validateAllContacts();
+
+  $logger = new SourceContactLogger();
+  $logger->printStats();
+}
+
+
+function mark_duplicates() {
+
+  $duplicateFinder = new SourceContactDuplicateFinder();
+  $duplicateFinder->markMainContacts();
+}
+
+
+function convert_contacts() {
+  global $BATCH_LIMIT;
+
+  $convertor = new Convertor($BATCH_LIMIT);
+  $convertor->start();
+}
+
+
+function convert_events() {
+
+}
+
 function getTask() {
   global $argv;
 
   if (count($argv) > 1) {
-    if ($argv[1] == 'score') {
-      return $argv[1];
-    }
-    elseif ($argv[1] == 'convert') {
+    if (isValidTask($argv[1])) {
       return $argv[1];
     }
     else {
-      echo "Please specify a task: {score|convert}\n";
-      exit(1);
+      showUsageAndExit();
     }
   }
   else {
-    echo "Please specify a task: {score|convert}\n";
-    exit(1);
+    showUsageAndExit();
   }
 }
 
-$task = getTask();
-main($task);
+function isValidTask($task) {
+  global $validTasks;
+
+  if (in_array($task, $validTasks)) {
+    return TRUE;
+  }
+  else {
+    return FALSE;
+  }
+}
+
+function showUsageAndExit() {
+  global $validTasks;
+
+  echo 'Please specify a valid task: {' . implode('|', $validTasks) . "}\n";
+  exit(1);
+}
+
+main();
