7aab7d77106f6496f98db6a7db167910d547dfdd
Alain Benbassat <alain@businessandcode.eu>
2022-03-10 13:53:58 +0100

    next

--- tools/classes/TargetGroup.php
+++ tools/classes/TargetGroup.php
@@ -2,8 +2,8 @@
 
 class TargetGroup {
   public function create($group) {
-    $newId = $this->createWithAPI($group);
-    $this->keepOriginalGroupId($group['id'], $newId);
+    $this->createWithOriginalId($group);
+    $this->addAdditionalInfo($group);
   }
 
   public function createGroupContact($groupId, $groupContactId) {
@@ -17,7 +17,27 @@ class TargetGroup {
     }
   }
 
+  private function createWithOriginalId($group) {
+    $sql = "insert into civicrm_group (id, name, title) values (%1, %2, %3)";
+    $sqlParams = [
+      1 => [$group['id'], 'Integer'],
+      2 => [$group['name'], 'String'],
+      3 => [$group['title'], 'String'],
+    ];
+    CRM_Core_DAO::executeQuery($sql, $sqlParams);
+  }
+
+  private function addAdditionalInfo($group) {
+    $result = civicrm_api3('Group', 'create', [
+      'id' => $group['id'],
+      'description' => $group['description'],
+      'is_active' => $group['is_active'],
+      'group_type' => [2],
+    ]);
+  }
+
   private function createWithAPI($group) {
+    // OBSOLETE
     $result = civicrm_api3('Group', 'create', [
       'name' => $group['name'],
       'title' => $group['title'],
@@ -30,6 +50,7 @@ class TargetGroup {
   }
 
   private function keepOriginalGroupId($oldId, $newId) {
+    // OBSOLETE
     if ($oldId != $newId) {
       $sql = "update civicrm_group set id = $oldId where id = $newId";
       CRM_Core_DAO::executeQuery($sql);
