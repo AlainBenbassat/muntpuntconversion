783028ad21a6935f5459f02b95d307b6e0b8dae5
Alain Benbassat <alain@businessandcode.eu>
2022-04-05 21:01:04 +0200

    next

--- tools/mpc_convert_message_templates.sh
+++ tools/mpc_convert_message_templates.sh
@@ -3,7 +3,10 @@
 LOCAL_ICONTACT_CREDENTIALS=settings/icontacts_local.cnf
 CIVI_CREDENTIALS=settings/civi.cnf
 
-mysqldump --skip-triggers --no-create-db --no-create-info --compact --skip-extended-insert --defaults-file="$LOCAL_ICONTACT_CREDENTIALS" db19666 \
+mysqldump \
+   --defaults-file="$LOCAL_ICONTACT_CREDENTIALS" \
+   --skip-triggers --no-create-db --no-create-info --compact --skip-extended-insert
+   db19666 \
   --tables civicrm_msg_template \
   --where="id in (343,373,375,377,379,399,411,419,421,423,426,443,444)" \
   | \
--- tools/mpc_copy_icontact_database.sh
+++ tools/mpc_copy_icontact_database.sh
@@ -39,6 +39,7 @@ mysqldump  --defaults-file="$REMOTE_ICONTACT_CREDENTIALS" -h 172.25.17.2 -P 3307
   civicrm_uf_join \
   civicrm_recurring_entity \
   civicrm_action_schedule \
+  civicrm_msg_template \
   civicrm_value_diensten_en_producten_4 \
   civicrm_value_media_info_12 \
   civicrm_value_medewerker_type_17 \
