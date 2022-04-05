fc0e45ff4d42a589c155aa1a1a11139245491eb6
Alain Benbassat <alain@businessandcode.eu>
2022-04-05 21:23:34 +0200

    next

--- tools/mpc_convert_message_templates.sh
+++ tools/mpc_convert_message_templates.sh
@@ -5,7 +5,12 @@ CIVI_CREDENTIALS=settings/civi.cnf
 
 mysqldump \
    --defaults-file="$LOCAL_ICONTACT_CREDENTIALS" \
-   --skip-triggers --no-create-db --no-create-info --compact --skip-extended-insert
+   --skip-triggers \
+   --no-create-db \
+   --no-create-info \
+   --compact \
+   --skip-extended-insert \
+   --complete-insert \
    db19666 \
   --tables civicrm_msg_template \
   --where="id in (343,373,375,377,379,399,411,419,421,423,426,443,444)" \
